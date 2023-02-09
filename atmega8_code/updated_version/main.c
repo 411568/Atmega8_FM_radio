a0d511fb915a4f13b4f36f26c4ca6048a5ec87c6
Krzysztof Sikora <krzysieksikora717@gmail.com>
2023-02-09 11:24:32 +0100

    tea5767 lib created

--- atmega8_code/updated_version/main.c
+++ atmega8_code/updated_version/main.c
@@ -7,62 +7,19 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "i2c_lib.c"
-
+#include "tea5767.c"
 
 
 #define _BV(bit)			(1 << (bit))
 #define bit_is_set(sfr, bit)		(_SFR_BYTE(sfr) & _BV(bit))
 #define bit_is_clear(sfr, bit)		(!(_SFR_BYTE(sfr) & _BV(bit)))
 
-#define TEA5767_ADDRESS_W               0b11000000	 //address for writing to the TEA5767 (0xC0)
+#define button_1_pin PC0
+#define button_1_port PINC
+#define button_2_pin PC1
+#define button_2_port PINC
 
 
-double radioFreq = PRESET1;
-uint8_t rcvCmd = 0;
-char msg[150];
-
-//global variables
-double frequency;
-
-//preset radio stations
-double frequencytab[] = {89.2, 91.5, 95.7, 96.4, 97.6, 99.7, 102.2, 104.5, 105, 106.7};
-
-
-// send the frequency to the TEA module
-void send_freq()
-{
-	unsigned char frequencyH;
-	unsigned char frequencyL;
-	unsigned int frequencyB;
-
-	frequencyB = 4 * (frequency * 1000000 + 225000) / 32768;
-	frequencyH = frequencyB >> 8;
-	frequencyL = frequencyB & 0xFF;
-
-    //data transmition 
-    I2C_Start();
-	I2C_Write(TEA5767_ADDRESS_W);
-	I2C_Write(frequencyH);
-	I2C_Write(frequencyL);
-	I2C_Write(0xB0);
-	I2C_Write(0x10);
-	I2C_Write(0x00);
-    I2C_Stop();
-
-    _delay_ms(10);
-}
-
-void setup()
-{
-    I2C_Init();
-
-    //starting frequency from the table
-	frequency = 89.2;
-	send_freq();
-
-	_delay_ms(100);
-}
 
 int main(void)
 {
@@ -70,6 +27,8 @@ int main(void)
 	setup();
 
 	//setup GPIO
+    DDRC = 0x00; //C pins as inputs 
+    PORTC = 0xFF; //C pins with internal pullup resistors
 
 
 
@@ -77,21 +36,25 @@ int main(void)
 	while(1)
 	{
 		// ------ SEARCH UP ------ //
-		if(bit_is_clear(PIND, button_1)) //change pin
+		if(bit_is_clear(button_1_port, button_1_pin)) 
 		{
 			_delay_ms(50); //debounce
-			if(bit_is_clear(PIND, button_1))
+			if(bit_is_clear(button_1_port, button_1_pin))
 			{
-				//search_up
+                search(1);
+                _delay_ms(500);
 			}
 		}
+
+
 		// ------ SEARCH DOWN ------ //
-		if(bit_is_clear(PIND, button_1)) //change pin
+		if(bit_is_clear(button_2_port, button_2_pin)) 
 		{
 			_delay_ms(50); //debounce
-			if(bit_is_clear(PIND, button_1))
+			if(bit_is_clear(button_2_port, button_2_pin))
 			{
-				//search_down
+                search(0);
+                _delay_ms(500);
 			}
 		}
 	}
---
+++ atmega8_code/updated_version/tea5767.c
@@ -0,0 +1,81 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "i2c_lib.c"
+
+#define TEA5767_ADDRESS_W               0b11000000	 //address for writing to the TEA5767 (0xC0)
+
+
+//global variables
+double frequency;
+
+//preset radio stations
+double frequencytab[] = {89.2, 91.5, 95.7, 96.4, 97.6, 99.7, 102.2, 104.5, 105, 106.7};
+
+
+
+// send the frequency to the TEA module
+void send_freq()
+{
+	unsigned char frequencyH;
+	unsigned char frequencyL;
+	unsigned int frequencyB;
+
+	frequencyB = 4 * (frequency * 1000000 + 225000) / 32768;
+	frequencyH = frequencyB >> 8;
+	frequencyL = frequencyB & 0xFF;
+
+    //data transmition 
+    I2C_Start();
+	I2C_Write(TEA5767_ADDRESS_W);
+	I2C_Write(frequencyH);
+	I2C_Write(frequencyL);
+	I2C_Write(0xB0);
+	I2C_Write(0x10);
+	I2C_Write(0x00);
+    I2C_Stop();
+
+    _delay_ms(10);
+}
+
+
+void search(uint8_t direction)
+{
+	unsigned char frequencyH;
+	unsigned char frequencyL;
+	unsigned int frequencyB;
+
+	frequencyB = 4 * (frequency * 1000000 + 225000) / 32768;
+	frequencyH = frequencyB >> 8;
+	frequencyL = frequencyB & 0xFF;
+
+    I2C_Start();
+	I2C_Write(TEA5767_ADDRESS_W); //send address for writing
+	I2C_Write(frequencyH |= 0b01000000); //search mode
+	I2C_Write(frequencyL);
+    if(direction == 1)
+    {
+        I2C_Write(0b11010000); //search up with mid adc 
+    }
+    else
+    {
+        I2C_Write(0b01010000); //search down with mid adc
+    }
+    I2C_Write(0b00010000); //basic ic setup
+    I2C_Write(0b00000000); //don't care
+
+}
+
+void setup()
+{
+    I2C_Init();
+
+    //starting frequency from the table
+	frequency = 89.2;
+	send_freq();
+
+	_delay_ms(100);
+}
+
